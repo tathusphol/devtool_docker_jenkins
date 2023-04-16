1b3cb231b43353e28d64a389db896443363aa26e
tathusphol <73683391+tathusphol@users.noreply.github.com>
2023-04-17 01:39:32 +0700

    Add files via upload

--- devtool_backend/config.js
+++ devtool_backend/config.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 require("dotenv").config()
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: "31.220.110.101",
+  user: "u350327849_devtool",
+  password: "!Po12345678",
+  database: "u350327849_devtool",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
